4401435b426aac1cdc8de54d8705ea79295592e7
Alexander Gorlenko <agorlenko4@gmail.com>
2023-10-09 21:50:55 +0300

    Added purchase recommendations

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Category, Product
 from cart.forms import CartAddProductForm
+from .recommender import Recommender
 
 
 def product_list(request, category_slug=None):
@@ -33,11 +34,17 @@ def product_detail(request, id, slug):
         available=True
     )
     cart_product_form = CartAddProductForm()
+    r = Recommender()
+    recommended_products = r.suggest_products_for(
+        products=[product],
+        max_results=4
+    )
     return render(
         request=request,
         template_name='shop/product/detail.html',
         context={
             'product': product,
-            'cart_product_form': cart_product_form
+            'cart_product_form': cart_product_form,
+            'recommended_products': recommended_products
         }
     )
